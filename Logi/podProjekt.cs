12c364ca193240147dcb64c2f4aa9f7290c811ae
QuiX23 <rav.sadowski@gmail.com>
2014-10-21 23:25:37 +0200

    podProjekt

---
+++ Logi/podProjekt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logi
+{
+    class podProjekt
+    {
+        public DateTime przydziel;
+        public DateTime ukoncz;
+        public string pesel;
+        public string nazwa;
+        public string opis;
+
+    }
+}
