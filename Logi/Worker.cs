c3ab9b128162356e06254db336a87bb582bcd10b
QuiX23 <rav.sadowski@gmail.com>
2014-10-22 00:51:39 +0200

    Workers

--- Logi/Worker.cs
+++ Logi/Worker.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Logi
 {
-    class Worker
+    class Worker : IEquatable<Worker>
     {
         public Worker()
         {
@@ -23,14 +23,86 @@ namespace Logi
 
 
 
-        public Worker(string pesel, string nazwisko, string imie, string stanowisko, int pensja, int etat)
+        public Worker(List<Worker> workers)
         {
-            this.etat = etat;
-            this.imie = imie;
-            this.nazwisko = nazwisko;
-            this.pensja = pensja;
-            this.pesel = pesel;
-            this.stanowisko = stanowisko;
+            Random rnd = new Random();
+
+            List<string> imiona = new List<string>();
+            imiona.Add("Adam");
+            imiona.Add("Marcin");
+            imiona.Add("Krzysztof");
+            imiona.Add("Tomasz");
+            imiona.Add("Rafał");
+            imiona.Add("Wojciech");
+            imiona.Add("Kamil");
+            imiona.Add("Agnieszka");
+            imiona.Add("Katarzyna");
+            imiona.Add("Anna");
+            imiona.Add("Monika");
+            imiona.Add("Patryk");
+            imiona.Add("Mateusz");
+            imiona.Add("Jacek");
+            imiona.Add("Adrian");
+            imie = imiona[rnd.Next(imiona.Count)];
+
+            List<string> nazwiska = new List<string>();
+            nazwiska.Add("Wolno");
+            nazwiska.Add("Konrad");
+            nazwiska.Add("Rozwadowski");
+            nazwiska.Add("Suryn");
+            nazwiska.Add("Kuta");
+            nazwiska.Add("Szwermer");
+            nazwiska.Add("Mordehaj");
+            nazwiska.Add("Nowak");
+            nazwiska.Add("Salnik");
+            nazwiska.Add("Mohamed");
+            nazwiska.Add("Gołota");
+            nazwiska.Add("Bednarek");
+            nazwiska.Add("Smith");
+            nazwiska.Add("Szmidka");
+            nazwiska.Add("Jelonek");
+            nazwiska.Add("Zabawa");
+            nazwisko = nazwiska[rnd.Next(nazwiska.Count)];
+
+            this.stanowisko = (rnd.Next(100) > 85) ? "team leader" : ((rnd.Next(100) > 80) ?  "stazysta" :  "teamleader");
+
+            if (this.stanowisko == "team leader")
+            {
+                etat = 40;
+                pensja = rnd.Next(50, 80)*100;
+            }
+            else if (this.stanowisko == "programista")
+            {
+                etat = rnd.Next(24, 40);
+                this.pensja = rnd.Next(3000,8000)*100;
+            }
+            else if (this.stanowisko == "stazysta")
+            {
+                this.etat = 24;
+                this.pensja = 2700;
+            }
+            
+            do
+            {
+                this.pesel = RandomData.randPesel();
+            } while (workers.Exists(x => x.pesel == this.pesel));
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            Worker objAsWorker = obj as Worker;
+            if (objAsWorker == null) return false;
+            else return Equals(objAsWorker);
+        }
+        public override int GetHashCode()
+        {
+            return this.pesel.GetHashCode();
+        }
+        public bool Equals(Worker other)
+        {
+            if (other == null) return false;
+            return (this.pesel.Equals(other.pesel));
         }
     }
 }
