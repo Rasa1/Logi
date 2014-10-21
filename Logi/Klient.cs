1d9dd67d2fa15982cda037bb5be2867c45bf72b4
ocenzurowano <skarpetownik@gmail.com>
2014-10-21 23:36:25 +0200

    klient

---
+++ Logi/Klient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logi
+{
+    class Klient
+    {
+        Klient()
+        {
+
+        }
+
+        public string nazwa;
+        public string nr_telefonu;
+        public string adres;
+
+        Klient(string nazwa,string nr_telefonu,string adres)
+        {
+            this.adres = adres;
+            this.nazwa = adres;
+            this.nr_telefonu = nr_telefonu;
+        }
+    }
+}
