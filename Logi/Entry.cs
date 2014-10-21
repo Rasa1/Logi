5d0b34df1454c5596ba8752d017fa8c4f380fac7
QuiX23 <rav.sadowski@gmail.com>
2014-10-21 20:43:35 +0200

    2

---
+++ Logi/Entry.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logi
+{
+    class Entry
+    {
+        public Entry()
+        {
+
+        }
+
+        public String pesel;
+        public DateTime entryDate;
+        public DateTime exitDate;
+
+        public Entry(String pesel, DateTime entryDate, DateTime exitDate)
+        {
+            this.pesel = pesel;
+            this.entryDate = entryDate;
+            this.exitDate = exitDate;
+       }
+
+        
+    }
+}
