5cfad52e618a1852be4d100d2de6b16e0d99c639
QuiX23 <rav.sadowski@gmail.com>
2014-10-22 02:38:36 +0200

    Klient-Project

--- Logi/Klient.cs
+++ Logi/Klient.cs
@@ -17,11 +17,47 @@ namespace Logi
         public string nr_telefonu;
         public string adres;
 
-        Klient(string nazwa,string nr_telefonu,string adres)
+        Klient(string nazwa,string adres,List <Project> projekty)
         {
+            Random rnd = new Random();
             this.adres = adres;
-            this.nazwa = adres;
-            this.nr_telefonu = nr_telefonu;
+            this.nazwa = nazwa;
+            this.nr_telefonu = rnd.Next(100000000,999999999).ToString();
         }
+
+        void genProj(List<Project> projekty,List<Worker> leaders)
+        {
+            Random rnd = new Random();
+            for (int i=0;i<rnd.Next(1,11);i++){
+             int wartosc_zamowienia=rnd.Next(100,1000)*100;
+             string nazwa="projekt"+this.nazwa+"num"+i.ToString();
+             string opis=nazwa.GetHashCode().ToString();
+             string pesel=leaders[rnd.Next(leaders.Count)].pesel;
+             string klient=this.nazwa;
+             List<string>tech=new List<string>();
+             
+             tech.Add("Java");
+             tech.Add(".NET");
+             tech.Add("ObjectiveC");
+             tech.Add("Ruby");
+             tech.Add("C++");
+             tech.Add("PHP");
+             tech.Add("Bash");
+             tech.Add("Html");
+            
+             string technologia=tech[rnd.Next(tech.Count)];
+             DateTime data_zamowienia=new DateTime(2014,rnd.Next(1,13),rnd.Next(1,30));
+
+             DateTime data_min=new DateTime(data_zamowienia.Year,data_zamowienia.Month,data_zamowienia.Day);
+             data_min.AddDays(15);
+             int range=(new DateTime(2014,12,30)-data_min).Days;
+             DateTime data_zakonczenia=new DateTime();
+             data_zakonczenia.AddDays(rnd.Next(range));
+
+             projekty.Add(new Project(wartosc_zamowienia, nazwa, opis, pesel, klient, technologia, data_zamowienia, data_zakonczenia));
+
+            }
+        }
+
     }
 }
--- Logi/Project.cs
+++ Logi/Project.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Logi
 {
-    class Project
+    class Project : IEquatable<Project>
     {
         Project()
         {
@@ -16,20 +16,39 @@ namespace Logi
         public int wartosc_zamowienia;
         public string nazwa;
         public string opis;
+        public string pesel;
         public string klient;
         public string technologia;
         public DateTime data_zakonczenia;
         public DateTime data_zamowienia;
 
-        Project(int wartosc_zamowienia,string nazwa,string opis,string klient,string technologia,DateTime data_zakonczenia,DateTime data_zamowienia)
+        public Project(int wartosc_zamowienia,string nazwa,string opis,string pesel,string klient,string technologia,DateTime data_zakonczenia,DateTime data_zamowienia)
         {
             this.data_zakonczenia = data_zakonczenia;
+            this.pesel = pesel;
             this.data_zamowienia = data_zamowienia;
             this.klient = klient;
             this.nazwa = nazwa;
             this.opis = opis;
             this.technologia = technologia;
             this.wartosc_zamowienia = wartosc_zamowienia;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            Project objAsProject = obj as Project;
+            if (objAsProject == null) return false;
+            else return Equals(objAsProject);
+        }
+        public override int GetHashCode()
+        {
+            return this.nazwa.GetHashCode();
+        }
+        public bool Equals(Project other)
+        {
+            if (other == null) return false;
+            return (this.nazwa.Equals(other.nazwa));
         }
     }
 
